48f0437478f58c94ea63c3ca76f7364e79b542c6
brandon <brandonwz6860@outlook.com>
2022-04-16 18:07:18 -0400

    add approx for relu + fix baseline model

--- effhe/models/baseline_relu_1c1f.py
+++ effhe/models/baseline_relu_1c1f.py
@@ -7,7 +7,7 @@ class SimpleConvNet(torch.nn.Module):
     def __init__(self, hidden=64, output=10):
         super(SimpleConvNet, self).__init__()        
         self.conv1 = torch.nn.Conv2d(1, 4, kernel_size=7, padding=0, stride=3)
-        self.fc1 = torch.nn.Linear(256, hidden)
+        self.fc1 = torch.nn.Linear(256, output)
         self.relu = torch.nn.ReLU()
 
     def forward(self, x):
