1db0470e7ab1fc48be88263dc6b5b1d8cddd077f
brandon <brandonwz6860@outlook.com>
2022-04-16 01:34:36 -0400

    add enc for 1c1f

---
+++ effhe/models/baseline_relu_1c1f_enc.py
@@ -0,0 +1,52 @@
+import tenseal as ts
+import torch
+
+class EncSimpleConvNet:
+    '''
+    Note that this requires context for init. However, in practice this is not
+    needed as the client will be the one decrypting and re-encrypting before sending
+    the result back to the model during the forward pass.
+    '''
+
+    def __init__(self, torch_nn, context):
+        self.conv1_weight = torch_nn.conv1.weight.data.view(
+            torch_nn.conv1.out_channels, torch_nn.conv1.kernel_size[0],
+            torch_nn.conv1.kernel_size[1]
+        ).tolist()
+        self.conv1_bias = torch_nn.conv1.bias.data.tolist()
+        
+        self.fc1_weight = torch_nn.fc1.weight.T.data.tolist()
+        self.fc1_bias = torch_nn.fc1.bias.data.tolist()
+
+        self.relu = torch.nn.ReLU()
+        self.context = context 
+        
+    def forward(self, enc_x, windows_nb):
+        # conv layer
+        enc_channels = []
+        for kernel, bias in zip(self.conv1_weight, self.conv1_bias):
+            y = enc_x.conv2d_im2col(kernel, windows_nb) + bias
+            enc_channels.append(y)
+
+        # pack all channels into a single flattened vector
+        enc_x = ts.CKKSVector.pack_vectors(enc_channels)
+
+        # =====BEGIN CLIENT-SIDE ACTIONS=====
+        # decrypt 
+        dec_x = torch.tensor(enc_x.decrypt())
+
+        # apply relu
+        dec_x = self.relu(dec_x)
+
+        # encrypt again
+        enc_x = ts.CKKSVector(self.context, dec_x)
+
+        #=====END CLIENT-SIDE ACTIONS=====
+
+        # fc1 layer
+        enc_x = enc_x.mm(self.fc1_weight) + self.fc1_bias
+
+        return enc_x
+    
+    def __call__(self, *args, **kwargs):
+        return self.forward(*args, **kwargs)
