915efa62a27902e6212f1cac47b1b3c93fc0bf74
Ketan Jog <kj2473@columbia.edu>
2022-04-28 16:30:11 -0400

    server listens

--- effhe/server-client/service.py
+++ effhe/server-client/service.py
@@ -9,10 +9,24 @@ import json
 
 PORT = 8080
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client = client.connect(('0.0.0.0', PORT))
 data = client.recv(1024)
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+server.bind((HOST, PORT))
+server.listen()
+conn, addr = server.accept()
+with conn:
+    print(f"Connection established by {addr}")
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+
 
 
 
