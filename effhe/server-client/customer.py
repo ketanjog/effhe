361b3e9f88fc7de48985cd59d12c62fa49b5c0ea
brandon <brandonwz6860@outlook.com>
2022-04-28 16:26:48 -0400

    customer.py

--- effhe/server-client/customer.py
+++ effhe/server-client/customer.py
@@ -1,12 +1,14 @@
 import socket
 import torch
 import json
+import tenseal as ts
 
 #This is the CUSTOMER file
 
 #They act as the server since they recieve a request, apply non-linearity, and send it back
 
 PORT = 8080
+IP = '0.0.0.0'
 
 def compute_act(act, inpt, params=None):
 	if(act == "relu"):
@@ -33,7 +35,7 @@ def get_data(server, byte_size):
 	data = json.loads(data.decode())
 	params = data['params']
 	tensor = data['tensor']
-	tensor = torch.Tensor(tensor)
+	tensor = torch.tensor(tensor)
 
 	return params, tensor
 
@@ -70,14 +72,42 @@ def gen_key(kind):
 
 def get_server():
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server.bind(('0.0.0.0', PORT))
+	server.bind((IP, PORT))
 	return server
 
-server = get_server()
-server.listen(5)
+def get_client():
+	client = client.socket(socket.AF_INET, socket.SOCK_STREAM)
+	return client
 
-while True:
-	sock, addr = server.accept()
+def send_data(client, data, context, kernel_shape, stride):
+	client.connect((IP, PORT))
+
+	data_enc, windows_nb = ts.im2col_encoding(
+            context, data.view(28, 28).tolist(), kernel_shape[0],
+            kernel_shape[1], stride
+    )
+
+	data_enc = data_enc.serialize()
+
+	payload = {
+		"data": data_enc
+	}
+
+	payload = json.dumps(payload)
+
+	client.send(payload)
+	client.close()
+
+#Part 1
+
+kernel_shape = (7, 7)
+stride = 3
+
+client = get_client()
+context = gen_key("small")
+send_data(client, None, context, kernel_shape, stride)
+
+
 
 
 
